d777afb239b372261dd800cb73681e04c4cf674a
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2025-02-13 07:58:45 +1100

    Configure publishing

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,11 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+import com.vanniktech.maven.publish.JavaLibrary
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     kotlin("jvm") version "2.1.10"
+    id("com.vanniktech.maven.publish") version "0.30.0" 
 }
 
 group = "me.sparky983.komponent"
 version = "0.1.0"
+description = "The Komponent Compiler"
 
 repositories {
     mavenCentral()
@@ -14,6 +19,37 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.1.10")
+}
+
+mavenPublishing {
+    configure(JavaLibrary(
+        javadocJar = JavadocJar.Javadoc(),
+        sourcesJar = true,
+    ))
+    signAllPublications()
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    pom {
+        description = project.description 
+            ?: throw IllegalStateException("Add a project description")
+        url = "https://github.com/Sparky983/komponent/"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://www.opensource.org/licenses/mit-license"
+            }
+        }
+        developers {
+            developer {
+                id = "Sparky983"
+            }
+        }
+        scm {
+            url = "https://github.com/Sparky983/komponent"
+            connection = "scm:git:git://github.com/Sparky983/komponent.git"
+            developerConnection = "scm:git:ssh://git@github.com/Sparky983/komponent.git"
+        }
+    }
 }
 
 tasks {
--- gradle-plugin/build.gradle.kts
+++ gradle-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
     kotlin("jvm") version "2.1.10"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "me.sparky983.komponent"
@@ -16,10 +17,15 @@ dependencies {
 }
 
 gradlePlugin {
+    website = "https://github.com/Sparky983/komponent"
+    vcsUrl = "https://github.com/Sparky983/komponent.git"
     plugins {
         create("komponent") {
             id = "me.sparky983.komponent.compiler"
             implementationClass = "me.sparky983.komponent.gradle.KomponentPlugin"
+            displayName = "Komponent Compiler"
+            description = "The Komponent Compiler"
+            tags = listOf("komponent")
         }
     }
 }
