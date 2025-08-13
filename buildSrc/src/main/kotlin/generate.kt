d9bae8121a0cab725252fcd3295106c62b6efedc
Sparky <87631423+Sparky983@users.noreply.github.com>
2025-08-14 07:12:21 +1000

    Make tags return created element (#15)

--- buildSrc/src/main/kotlin/generate.kt
+++ buildSrc/src/main/kotlin/generate.kt
@@ -20,6 +20,7 @@ data class Global(
 
 @Serializable
 data class Tag(
+    val type: String = "HTMLElement",
     val attributes: Map<String, Attribute> = mapOf(),
     val events: Map<String, Event> = mapOf(),
     val void: Boolean = false
@@ -136,8 +137,8 @@ fun generate(folder: File) {
             }
 
             append("\n")
-            append(") {\n")
-            append("    tag(\n")
+            append("): ${tag.type} {\n")
+            append("    return tag(\n")
             append("        \"$name\",\n")
             append("        buildMap {")
 
@@ -197,7 +198,7 @@ fun generate(folder: File) {
                 append("    ) {}")
             }
 
-            append("\n}")
+            append(" as ${tag.type}\n}")
         }
     }
 
