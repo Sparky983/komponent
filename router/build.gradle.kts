f292eb1bf27e9c2d7bbfe8f09823b727aee8b82f
Sparky <87631423+Sparky983@users.noreply.github.com>
2026-01-15 20:57:13 +1100

    Add basic client-side routing (#16)

---
+++ router/build.gradle.kts
@@ -0,0 +1,61 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import com.vanniktech.maven.publish.KotlinMultiplatform
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform") version "2.1.10"
+    id("com.vanniktech.maven.publish") version "0.30.0"
+    id("org.jetbrains.dokka") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    explicitApi()
+    sourceSets.jsMain.dependencies {
+        implementation(project(":"))
+    }
+    js {
+        browser {}
+        binaries.executable()
+        compilerOptions {
+            target = "es2015"
+        }
+    }
+}
+
+mavenPublishing {
+    configure(KotlinMultiplatform(
+        javadocJar = JavadocJar.Dokka("dokkaHtml"),
+        sourcesJar = true
+    ))
+    signAllPublications()
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    pom {
+        description = project.description
+        url = "https://github.com/Sparky983/komponent"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://www.opensource.org/licenses/mit-license"
+            }
+        }
+        developers {
+            developer {
+                id = "Sparky983"
+            }
+        }
+        scm {
+            url = "https://github.com/Sparky983/komponent"
+            connection = "scm:git:git://github.com/Sparky983/komponent.git"
+            developerConnection = "scm:git:ssh://git@github.com/Sparky983/komponent.git"
+        }
+    }
+}
+
